58362fda827eb928940fb8a6ca64405d88b5eb0a
davidhieber <david.hieber@uni-tuebingen.de>
2024-01-10 10:19:14 +0100

    fix: refactor test

---
+++ test-central-api/test_master_Image_group_api.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+from dotenv import load_dotenv
+from yarl import URL
+import asyncio
+from hub_python_client import MasterImageGroupAPI
+
+
+class TestMasterImageGroupAPI(unittest.TestCase):
+    def setUp(self):
+        load_dotenv()
+        username = os.getenv('USERNAME_ROBOT')
+        password = os.getenv('PASSWORD_ROBOT')
+        base_url = URL(os.getenv('BASE_URL'))
+        self.loop = asyncio.get_event_loop()
+
+        self.api = MasterImageGroupAPI(username, password, base_url)
+
+    def test_master_many_image_groups(self):
+        self.loop.run_until_complete(self.api.get_many())
+        self.loop.run_until_complete(self.api.close())
+
+    def test_master_one_image_group(self):
+        master_image_groups = self.loop.run_until_complete(self.api.get_many())
+        print(master_image_groups)
+        self.loop.run_until_complete(self.api.get_one(master_image_groups['data'][0]['id']))
+        self.loop.run_until_complete(self.api.close())
+
+
+if __name__ == '__main__':
+    unittest.main()
